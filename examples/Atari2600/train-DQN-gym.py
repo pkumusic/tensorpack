8f92e5a4e4d3fbbe647dce2d28512ed258ad2b3a
lyzmusic <635716260@qq.com>
2016-11-02 21:33:01 -0400

    - Writing model

--- examples/Atari2600/train-DQN-gym.py
+++ examples/Atari2600/train-DQN-gym.py
@@ -5,7 +5,7 @@ import argparse
 import os
 from tensorpack.predict.common import PredictConfig
 from tensorpack import *
-from tensorpack.models.model_desc import ModelDesc
+from tensorpack.models.model_desc import ModelDesc, InputVar
 from tensorpack.train.config import TrainConfig
 from tensorpack.tfutils.common import *
 from tensorpack.callbacks.group import Callbacks
@@ -17,11 +17,35 @@ STEP_PER_EPOCH = 6000
 
 IMAGE_SIZE = (84, 84)
 FRAME_HISTORY = 4
+IMAGE_SHAPE3 = IMAGE_SIZE + (FRAME_HISTORY, ) # one state input
 
+
+NUM_ACTIONS = 4 #TODO: Generate it automatically later.
 
 
 class Model(ModelDesc):
     def _get_input_vars(self):
+
+        return [InputVar(tf.float32, (None,) + IMAGE_SHAPE3, 'state'),
+                InputVar(tf.int64, (None,), 'action'),
+                InputVar(tf.float32, (None,), 'reward'),
+                InputVar(tf.float32, (None,) + IMAGE_SHAPE3, 'next_state'),
+                InputVar(tf.bool, (None,), 'isOver')]
+        pass
+
+    def _get_DQN_prediction(self, image):
+        pass
+
+    def _build_graph(self, inputs):
+        state, action, reward, next_state, isOver = inputs
+        predict_value = self._get_DQN_prediction() # N * NUM_ACTIONS #TODO: If we need self. here
+        action_onehot = tf.one_hot(action, NUM_ACTIONS, 1.0, 0.0) # N * NUM_ACTION
+        pred_action_value = tf.reduce_sum(predict_value * action_onehot, 1) # N,
+
+
+
+        pass
+
 
 
 
